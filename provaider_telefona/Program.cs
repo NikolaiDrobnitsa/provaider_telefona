6639b51e4b81a3b5032c7b67c5cdf48d18cca3c4
Микола Дробница <nikolaidrobnitsa@gmail.com>
2022-04-09 00:42:34 +0300

    Добавьте файлы проекта.

---
+++ provaider_telefona/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace provaider_telefona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
